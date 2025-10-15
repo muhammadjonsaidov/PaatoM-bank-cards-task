234fbc86fc74b18a811968e41894fac972748761
muhammadjonsaidov <javascript0222@gmail.com>
2025-10-15 16:36:55 +0500

    ✨ feat(core): Add entities, repositories, and initial DB migration

---
+++ src/main/java/com/example/bankcards/entity/Card.java
@@ -0,0 +1,39 @@
+package com.example.bankcards.entity;
+
+import com.example.bankcards.entity.enums.CardStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "cards")
+@Getter
+@Setter
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Karta raqami shifrlangan holda saqlanadi
+    @Column(name = "card_number", nullable = false, unique = true)
+    private String cardNumber;
+
+    @Column(name = "owner_name", nullable = false)
+    private String ownerName;
+
+    @Column(name = "expiry_date", nullable = false)
+    private LocalDate expiryDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CardStatus status;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
---
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.bankcards.entity;
+
+import com.example.bankcards.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Card> cards;
+}
